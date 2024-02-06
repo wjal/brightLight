5fd67cab1504a342eee196c5a903ee0cccd2e681
wjal <wjalittle@myseneca.ca>
2024-02-06 17:45:46 -0500

    edit to nav buttons for mouseUp/Down fade

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,28 @@ const NavBar = ({contactClick, aboutClick, videoClick}) => {
         <div className="navbar">
             <img src="logo.png" className="logo" alt="logo" />
             <div className="navbar-menu">
-                <button className='menu-button' onClick={videoClick}>Products</button>
-                <button className='menu-button' onClick={aboutClick}>About Us</button>
-                <button className='menu-button' onClick={contactClick}>Contact Us</button>
+                <button className='menu-button' onClick={videoClick} onMouseDown={(e) => {
+                    const target = e.target;
+                    target.style.animation = '';
+                    target.style.color = 'rgb(231, 104, 87)';
+                }} onMouseUp={(e) => {
+                    const target = e.target;
+                    target.style.color = 'white';
+                    target.style.animation = 'fade .6s linear';}}>Products</button>
+                <button className='menu-button' onClick={aboutClick} onMouseDown={(e) => {
+                    const target = e.target;
+                    target.style.animation = '';
+                    target.style.color = 'rgb(231, 104, 87)';}} onMouseUp={(e) => {
+                        const target = e.target;
+                        target.style.color = 'white';
+                        target.style.animation = 'fade .6s linear';}}>About Us</button>
+                <button className='menu-button' onClick={contactClick} onMouseDown={(e) => {
+                    const target = e.target;
+                    target.style.animation = '';
+                    target.style.color = 'rgb(231, 104, 87)';}} onMouseUp={(e) => {
+                        const target = e.target;
+                        target.style.color = 'white'
+                        target.style.animation = 'fade .6s linear';}} >Contact Us</button>
             </div>
         </div>
     )
